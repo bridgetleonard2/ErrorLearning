5dfb6811f318cf38b3c30c11c776800d21bcb92f
Bridget Leonard <leonardbridget01@gmail.com>
2024-01-08 15:39:18 -0800

    add focus to textbox

--- shiny_app/error_learning/server.R
+++ shiny_app/error_learning/server.R
@@ -11,25 +11,6 @@ library(shiny)
 # Data manipulation and analysis
 library(dplyr)
 
-# Define word pairs and conditions
-word_pairs <- data.frame(
-  cue = c("PORTRAY", "PRESCRIPTION", "PARCEL", "CANYON", "LATIN", "STERN", "DRACULA", "ROBIN", "WELL", "INTRODUCE",
-          "GLIDE", "ORDER", "COURAGEOUS", "HONEYMOON", "LAUNDRY", "MEASUREMENT", "DANCER", "FUGITIVE", "CHIMNEY", "EYES",
-          "CRUNCH", "EMPIRE", "SAIL", "TEA", "SIDE", "DIAMETER", "CHUNK", "SAFARI", "ELEVATOR", "SWING",
-          "TASTY", "NITROGEN", "MODEL", "FILTHY", "CRITIC", "HUT", "FREEWAY", "ASSISTANCE", "SALES", "ANCESTOR",
-          "BISCUIT", "LIE", "BLOCKADE", "HOTEL", "USURP", "NOODLES", "EVICT", "COOKOUT", "UNCOMMON", "NATURE",
-          "THEOREM", "VISIT", "ANNUAL", "SINK", "STAR", "GRADUATE", "SOUTH", "VALLEY", "BATTERY", "ENERGY"),
-  target = c("SHOW", "PILL", "BOX", "MOUNTAIN", "FRENCH", "BOAT", "SCARY", "RED", "DONE", "PRESENT",
-             "HANG", "FORM", "LION", "ROMANCE", "SOAP", "HEIGHT", "MUSIC", "RUN", "BRICK", "COLOR",
-             "NOISE", "STATE", "OCEAN", "LEAVES", "ORDER", "LENGTH", "BLOCK", "DESERT", "BUILDING", "TREE",
-             "TREAT", "CHEMISTRY", "CAR", "DIRT", "ANALYZE", "ISLAND", "ROAD", "HELPER", "CLOTHES", "FAMILY", 
-             "COOKIE", "STEAL", "WALL", "BED", "TAKE", "CHICKEN", "APARTMENT", "HAMBURGER", "UNIQUE", "FOREST",
-             "GEOMETRY", "LEAVE", "PICNIC", "HOLE", "NIGHT", "CAP", "WEST", "LOW", "RADIO", "SUN"),
-  condition = c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-                2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2)
-)
-
 
 # Define server logic required to draw a histogram
 # Define server logic required to draw a histogram
@@ -83,12 +64,11 @@ function(input, output, session) {
   observeEvent(input$responsesObject, {
     # Do something with the received responsesObject
     responses$values <- input$responsesObject
-    print(responses$values)
     
     # Example: Extracting cues and responses
     cues <- names(responses$values)
-    responses_list <- lapply(responses$values, function(x) x[1])
-    response_times_list <- lapply(responses$values, function(x) x[2])
+    responses_list <- sapply(responses$values, function(x) x[1])
+    response_times_list <- sapply(responses$values, function(x) x[2])
     
     print(cues)
     print(responses_list)
@@ -110,6 +90,9 @@ function(input, output, session) {
       cue <- word_pairs$cue[1]
       target <- word_pairs$target[1]
       shinyjs::runjs(sprintf("shinyjs.updateTest('%s', '%s')", cue, target))
+    } else {
+      # Clear the text box
+      shinyjs::runjs('$("#answerTestContainer").empty()')
     }
   })
   
@@ -132,12 +115,11 @@ function(input, output, session) {
   observeEvent(input$answerObject, {
     # Do something with the received responsesObject
     answers$values <- input$answerObject
-    print(answers$values)
     
     # Example: Extracting cues and responses
     cues_test <- names(answers$values)
-    answers_list <- lapply(answers$values, function(x) x[1])
-    answers_times_list <- lapply(answers$values, function(x) x[2])
+    answers_list <- sapply(answers$values, function(x) x[1])
+    answers_times_list <- sapply(answers$values, function(x) x[2])
     
     print(cues_test)
     print(answers_list)
